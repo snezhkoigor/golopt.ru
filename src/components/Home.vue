481463fcbe1f63fb2fa13ba6d3d06cae110ac24b
igorsnezko <i.s.sergeevich@yandex.ru>
2017-12-10 15:40:44 +0300

    new home design

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,11 +2,11 @@
     <v-container fluid  grid-list-md class="pa-0">
         <v-parallax src="./src/assets/images/top.jpg" style="margin-top: -40px;" height="600">
             <v-layout column align-center justify-center>
-                <h3 style="position: absolute; top: 250px; font-size: 48px; font-family: 'Roboto', sans-serif" class="white--text parallax-text" v-html="$t('Volume indicator and options online')" />
-				<p style="position: absolute; bottom: 100px; font-size: 22px; font-family: 'Roboto', sans-serif">
+                <h3 style="position: absolute; top: 150px; font-size: 48px; font-family: 'Roboto', sans-serif" class="white--text parallax-text" v-html="$t('Volume indicator and options online')" />
+				<p style="position: absolute; bottom: 150px; font-size: 22px; font-family: 'Roboto', sans-serif">
 					{{ $t('Exclusive data analysis from CME to MT4') }}
 				</p>
-                <p style="position: absolute; bottom: 20px;">
+                <p style="position: absolute; bottom: 70px;">
                     <v-btn
                             color="info"
                             large
