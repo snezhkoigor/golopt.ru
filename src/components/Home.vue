3d67a3e4942a028e46e1650dc99f2a0fcd46a815
igorsnezko <i.s.sergeevich@yandex.ru>
2017-12-10 16:20:05 +0300

    new home design

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -196,7 +196,15 @@
 		</v-layout>
 
 		<v-parallax jumbotron src="./src/assets/images/footer.jpg" height="300" style="margin-bottom: -20px">
-			<v-layout column align-center justify-center style="font-size: 22px; font-family: 'Roboto', sans-serif">
+			<v-layout class="hidden-md-and-down" column align-center justify-center style="font-size: 22px; font-family: 'Roboto', sans-serif">
+				<p class="text-lg-center parallax-text" v-html="$t('Footer feedback')">
+				</p>
+				<p class="text-lg-center parallax-text">
+					<router-link :to="{ name: 'feedback'}" replace style="color: white">{{ $t('Feedback') }}</router-link>
+				</p>
+			</v-layout>
+
+			<v-layout column align-center justify-center style="font-size: 18px; font-family: 'Roboto', sans-serif">
 				<p class="text-lg-center parallax-text" v-html="$t('Footer feedback')">
 				</p>
 				<p class="text-lg-center parallax-text">
