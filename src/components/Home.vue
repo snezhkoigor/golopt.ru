15859c9e6608f22102acaee0a00687e93ff30db0
igorsnezko <i.s.sergeevich@yandex.ru>
2017-12-11 13:15:01 +0300

    new home design

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,9 +8,9 @@
 				<p class="hidden-md-and-down" style="position: absolute; bottom: 200px; font-size: 22px; font-family: 'Roboto', sans-serif">
 					{{ $t('Exclusive data analysis from CME to MT4') }}
 				</p>
-				<!--<p class="hidden-lg-and-up text-md-center" style="position: absolute; bottom: 200px; font-size: 16px; font-family: 'Roboto', sans-serif">-->
-					<!--{{ $t('Exclusive data analysis from CME to MT4') }}-->
-				<!--</p>-->
+				<p class="hidden-lg-and-up text-md-center" style="text-align: center; position: absolute; bottom: 180px; font-size: 16px; font-family: 'Roboto', sans-serif">
+					{{ $t('Exclusive data analysis from CME to MT4') }}
+				</p>
 
                 <p style="position: absolute; bottom: 100px;">
                     <v-btn
