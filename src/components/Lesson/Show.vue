0ab8758d4fc8b55c8c721059e66dbb32d5fb39f4
Igor Snezhko <i.s.sergeevich@yandex.ru>
2018-09-26 13:19:03 +0300

    Create Show.vue

---
+++ src/components/Lesson/Show.vue
@@ -0,0 +1,38 @@
+<template>
+	<v-layout row>
+		<v-flex xs12 v-if="!pending">
+			<v-btn class="green--text darken-1" flat="flat" @click="goToViewLessons()">{{ $t('Back') }}</v-btn>
+			<v-card class="grey lighten-4 elevation-0" v-if="this.lesson.text.length">
+				<v-card-text v-html="this.lesson.text" />
+			</v-card>
+		</v-flex>
+	</v-layout>
+</template>
+
+<script>
+    import { mapGetters, mapActions } from 'vuex';
+
+    export default {
+        data() {
+            return {}
+        },
+        mounted() {
+            this.show(parseInt(this.$route.params.lessonId));
+        },
+        computed: {
+            ...mapGetters('Lesson', [
+                'lesson', 'pending'
+            ])
+        },
+        methods: {
+            ...mapActions('Lesson', [
+                'show'
+            ]),
+            goToViewLessons() {
+                this.$router.push({
+                    name: 'lessonView'
+                });
+			}
+        }
+    }
+</script>
