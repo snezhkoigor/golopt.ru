cff68134538457e20ee6d81bbb03c30cd4019f69
Igor Snezhko <i.s.sergeevich@yandex.ru>
2018-09-27 12:30:39 +0300

    Update Show.vue

--- src/components/Lesson/Show.vue
+++ src/components/Lesson/Show.vue
@@ -3,7 +3,9 @@
 		<v-flex xs12 v-if="!pending">
 			<v-btn class="green--text darken-1" flat="flat" @click="goToViewLessons()">{{ $t('Back') }}</v-btn>
 			<v-card class="grey lighten-4 elevation-0" v-if="this.lesson.text.length">
-				<v-card-text v-html="this.lesson.text" />
+				<v-card-text>
+					<div v-html="this.lesson.text"></div>
+				</v-card-text>	
 			</v-card>
 		</v-flex>
 	</v-layout>
